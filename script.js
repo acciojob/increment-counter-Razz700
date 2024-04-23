8969454bb110edb09d3e32a1a5396f9ae46f5e2f
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-23 09:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //your JS code here. If required.
-let body=document.getElementsByTagName("body");
+let body=document.getElementById("body");
 let p1=document.createElement("p");
 p1.setAttribute("id","counter");
 p1.textContent="0";
 body.appendChild(p1);
+console.log(p1);
 let btn=document.createElement("button");
 btn.textContent="Increment";
 btn.setAttribute("id","incrementBtn");
+body.appendChild(btn);
 btn.addEventListener("click",myfunc);
 let i=0;
 function myfunc(){
